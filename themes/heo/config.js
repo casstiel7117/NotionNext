b7910377990bc4fd7024aad589eee46d41afc4c9
casstiel7117 <164290229+casstiel7117@users.noreply.github.com>
2024-05-06 22:40:32 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -23,9 +23,9 @@ const CONFIG = {
   HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: '通达信指标', url: '/tag/通达信指标' },
+  HEO_HERO_CATEGORY_2: { title: '文华财经指标', url: '/tag/文华财经指标' },
+  HEO_HERO_CATEGORY_3: { title: '量化交易', url: '/tag/量化交易' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
