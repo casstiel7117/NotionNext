6a3c7d5943796efecdae66291a6436485a620973
casstiel7117 <164290229+casstiel7117@users.noreply.github.com>
2024-08-06 23:09:19 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -17,10 +17,10 @@ const CONFIG = {
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '分享量化知识',
   HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'NEWJQK.TOP',
+  HEO_HERO_TITLE_3: 'BLOG.AYPY.TOP',
   HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://newjqk.top',
+  HEO_HERO_TITLE_5: '指标答疑|每周更新',
+  HEO_HERO_TITLE_LINK: 'https://blog.aypy.top',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '通达信指标', url: '/tag/通达信指标' },
