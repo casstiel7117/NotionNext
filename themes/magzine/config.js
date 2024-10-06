0e8d895a5aea1b439688e155d35918e3a3925cef
casstiel7117 <164290229+casstiel7117@users.noreply.github.com>
2024-10-06 18:36:56 +0800

    Update config.js

--- themes/magzine/config.js
+++ themes/magzine/config.js
@@ -11,8 +11,7 @@ const CONFIG = {
   MAGZINE_HOME_DESCRIPTION:
     '借助NotionNext，获得助您开创、经营和扩展业务所需的全部工具和帮助。',
   MAGZINE_HOME_TIPS: 'AI时代来临，这是属于超级个体的狂欢盛宴！',
-  MAGZINE_HOME_TITLE_LINK: 'https://www.baidu.com',
-
+ 
   // 首页底部推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   MAGZINE_RECOMMEND_POST_TAG: '推荐',
   MAGZINE_RECOMMEND_POST_COUNT: 6,
@@ -35,7 +34,7 @@ const CONFIG = {
   MAGZINE_SOCIAL_CARD_TITLE_1: '交流频道',
   MAGZINE_SOCIAL_CARD_TITLE_2: '加入社群讨论分享',
   MAGZINE_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  MAGZINE_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/chat-community',
+  MAGZINE_SOCIAL_CARD_URL: 'https://www.baidu.com',
 
   // 页脚菜单
   MAGZINE_FOOTER_LINKS: [
